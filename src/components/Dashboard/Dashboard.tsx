2a55362a0fd74d89be71f810927ad4162659f468
mahdi abdulkareem <mahdiimad64@gmail.com>
2020-11-01 14:16:13 +0200

    route from url

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import AppHeader from "../AppHeader/AppHeader";
 import CatagoryList from "../CatagoryList/CatagoryList";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function Dashboard() {
   return (
+    // <Router>
     <div>
       <AppHeader />
-      <h1>Dashboard</h1>
-      <CatagoryList />
+      <Switch>
+        
+        <Route path="/dashboard">
+          <h1>Dashboard</h1>
+        </Route>
+      </Switch>
     </div>
+    //  </Router>
   );
 }
 
--- src/components/Routers/Routes.tsx
+++ src/components/Routers/Routes.tsx
@@ -2,18 +2,24 @@ import React from "react";
 import LoginPage from "../LoginPage/LoginPage";
 import Dashboard from "../Dashboard/Dashboard";
 import {  Switch, Route } from "react-router-dom";
+import CatagoryList from "../CatagoryList/CatagoryList";
+import AppHeader from "../AppHeader/AppHeader";
+
 function Routes() {
   return (
     <div>
-     
+      <AppHeader />
         <Switch>
-          <Route path="/" exact>
+          <Route path="/login" exact>
             <LoginPage />
           </Route>
 
           <Route path="/dashboard">
             <Dashboard />
           </Route>
+          <Route path="/category">
+          <CatagoryList />
+        </Route>
         </Switch>
      
     </div>
