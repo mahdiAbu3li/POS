9a99dfc34d08cfa41044e5a9a56520240c64f8a4
mahdi abdulkareem <mahdiimad64@gmail.com>
2020-11-28 02:39:47 +0200

    make Drwstyle file

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -27,104 +27,9 @@ import ExpandLess from "@material-ui/icons/ExpandLess";
 import ExpandMore from "@material-ui/icons/ExpandMore";
 import Collapse from "@material-ui/core/Collapse";
 import { useHistory } from "react-router-dom";
-
-import {
-  makeStyles,
-  useTheme,
-  Theme,
-  createStyles,
-} from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-
-const drawerWidth = 240;
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: "flex",
-    },
-    drawer: {
-      [theme.breakpoints.up("sm")]: {
-        width: drawerWidth,
-        flexShrink: 0,
-      },
-    },
-    appBar: {
-      [theme.breakpoints.up("sm")]: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-      },
-      backgroundColor: "#fff",
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-      [theme.breakpoints.up("sm")]: {
-        display: "none",
-      },
-    },
-    // necessary for content to be below app bar
-    toolbar: theme.mixins.toolbar,
-    drawerPaper: {
-      width: drawerWidth,
-      backgroundColor: "#fff",
-      "& > *": {
-        color: "#283593",
-      },
-    },
-    content: {
-      flexGrow: 1,
-      padding: theme.spacing(3),
-    },
-    barIcon: {
-      color: "#283593",
-      fontWeight: "bold",
-      fontSize: "1rem",
-      fontFamily: '"Sansita Swashed", cursive',  
-    },
-    barButton: {
-      paddingLeft: "0",
-      color: "#283593",
-      fontWeight: "bold",
-      "& :hover": {
-        color: "#d50000",
-      },
-    },
-    leftList: {
-      display: "flex",
-    },
-    list: {
-      display: "flex",
-      justifyContent: "space-between",
-      padding: "10px",
-      width: "100%",
-    },
-    rightList: {
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center",
-      width: "165px",
-    },
-
-    avatar: {
-      marginRight: "90px",
-    },
-    nested: {
-      paddingLeft: theme.spacing(4),
-    },
-    lists: {
-      "& > *": {
-        "& > *": {
-          color: "#283593",
-        },
-      },
-    },
-    corner:{
-        display:"flex",
-        justifyContent:"center",
-        alignItems:"center",
-    }
-  })
-);
+import { useStyles } from "./DrawerStyle";
+import { useTheme } from "@material-ui/core/styles";
 
 interface Props {
   onLogout: () => void;
@@ -151,9 +56,9 @@ export default function ResponsiveDrawer(props: Props) {
 
   const drawer = (
     <div>
-      <div className={[classes.toolbar , classes.corner].join(" ")}>
+      <div className={[classes.toolbar, classes.corner].join(" ")}>
         <Button color="inherit" className={classes.barIcon}>
-          MAHDI-SHOP
+          Mahdi-Shop
         </Button>
       </div>
 
---
+++ src/components/Drawer/DrawerStyle.ts
@@ -0,0 +1,90 @@
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+const drawerWidth = 240;
+
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    drawer: {
+      [theme.breakpoints.up("sm")]: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
+    },
+    appBar: {
+      [theme.breakpoints.up("sm")]: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+      },
+      backgroundColor: "#fff",
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up("sm")]: {
+        display: "none",
+      },
+    },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: drawerWidth,
+      backgroundColor: "#fff",
+      "& > *": {
+        color: "#283593",
+      },
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+    barIcon: {
+      color: "#283593",
+      fontWeight: "bold",
+      fontSize: "1.5rem",
+      fontFamily: '"Sansita Swashed", cursive',
+    },
+    barButton: {
+      paddingLeft: "0",
+      color: "#283593",
+      fontWeight: "bold",
+      "& :hover": {
+        color: "#d50000",
+      },
+    },
+    leftList: {
+      display: "flex",
+    },
+    list: {
+      display: "flex",
+      justifyContent: "space-between",
+      padding: "10px",
+      width: "100%",
+    },
+    rightList: {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      width: "165px",
+    },
+
+    avatar: {
+      marginRight: "90px",
+    },
+    nested: {
+      paddingLeft: theme.spacing(4),
+    },
+    lists: {
+      "& > *": {
+        "& > *": {
+          color: "#283593",
+        },
+      },
+    },
+    corner: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  })
+);
